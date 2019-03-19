06560010bd73fedce38333d34ff8cb31036e5619
Eagan Martin <mece.martinece@gmail.com>
2019-03-19 14:54:38 +0800

    Added comment for sample usage of component.reference

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ module.exports = {
 		query: (queryString) => {
 			return Ext.ComponentQuery.query(queryString)[0];
 		},
+		/**
+		 * * Sample Usage:
+		 * * Kony.component.reference('button', 'button-reference-value');
+		 */
 		reference: (component, reference) => {
 			return Ext.ComponentQuery.query(`${component}[reference = ${reference}]`)[0];
 		}
